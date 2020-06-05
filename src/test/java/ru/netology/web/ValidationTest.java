f48bbdba20b9919f1a8f1c042a981be3d31903b0
elenaslavutina <elena.slavutina2013@gmail.com>
2020-06-05 21:57:59 +0300

    validation tests are added

--- src/test/java/ru/netology/web/ValidationTest.java
+++ src/test/java/ru/netology/web/ValidationTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CallbackTest {
+class ValidationTest {
     private WebDriver driver;
 
     @BeforeAll
@@ -32,30 +32,28 @@ class CallbackTest {
         driver = null;
     }
 
-    @Test
-    void shouldTestV1() {
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Василий Пупкин");
-        elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
-
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
-    }
 
     @Test
-    void shouldTestV2() {
+    void shouldNotPassedIfNameNotValid1() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Василий Пупкин");
-        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+79270000000");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Василий_Пупкин");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        String text = driver.findElements(By.className("input__sub")).get(0).getText();
 
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
     }
 
+    @Test
+    void shouldNotPassedIfPhoneNotValid1() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Василий Пупкин");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("79270000000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElements(By.className("input__sub")).get(1).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
 
+  
 }
