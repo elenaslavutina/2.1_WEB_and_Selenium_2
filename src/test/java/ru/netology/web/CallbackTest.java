6b80c31018ace3fd56e6561e6cb6f7a02a537842
elenaslavutina <elena.slavutina2013@gmail.com>
2020-06-09 18:26:15 +0300

    try to make tests with selenide

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.codeborne.selenide.Selenide.*;
+
 
 import java.util.List;
 
@@ -21,7 +24,7 @@ class CallbackTest {
         System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
     }
 
-    @BeforeEach
+/*    @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
     }
@@ -31,31 +34,19 @@ class CallbackTest {
         driver.quit();
         driver = null;
     }
-
+*/
     @Test
     void shouldTestV1() {
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Василий Пупкин");
-        elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        open("http://localhost:9999");
+        $("form.form").$("[data-test-id=name]").sendKeys("Василий Пупкин");
+       $("[data-test-id=phone] input").sendKeys("+71231234123");
+       $("[data-test-id=agreement]").click();
+        $("button.button").click();
+    //    String actual = $("[data-test-id=order-success]").getText();
 
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actual.trim());
     }
 
-    @Test
-    void shouldTestV2() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Василий Пупкин");
-        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+79270000000");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
-
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
-    }
 
 
 }
--- src/test/java/ru/netology/web/ValidationTest.java
+++ src/test/java/ru/netology/web/ValidationTest.java
@@ -11,7 +11,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 
+import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
 
 class ValidationTest {
     private WebDriver driver;
@@ -24,6 +29,8 @@ class ValidationTest {
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
+
+
     }
 
     @AfterEach
@@ -32,28 +39,28 @@ class ValidationTest {
         driver = null;
     }
 
-
+/*
     @Test
     void shouldNotPassedIfNameNotValid1() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Василий_Пупкин");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElements(By.className("input__sub")).get(0).getText();
-
-        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+        open("http://localhost:9999");
+        $("form.form").$("[data-test-id=name] input").sendKeys("Василий_Пупкин");
+        $("form.form").$("[data-test-id=phone] input").sendKeys("+71231234123");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        String actual = $("[data-test-id=name]").$("[.input_invalid]").$(".input__sub").getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", actual.trim());
     }
 
     @Test
     void shouldNotPassedIfPhoneNotValid1() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Василий Пупкин");
-        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("79270000000");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElements(By.className("input__sub")).get(1).getText();
-        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+        open("http://localhost:9999");
+        $("form.form").$("[data-test-id=name] input").sendKeys("Василий Пупкин");
+        $("form.form").$("[data-test-id=phone] input").sendKeys("71231234123");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        String actual = $("[data-test-id=phone]").$("[.input_invalid]").$(".input__sub").getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", actual.trim());
     }
 
-  
+*/
 }
