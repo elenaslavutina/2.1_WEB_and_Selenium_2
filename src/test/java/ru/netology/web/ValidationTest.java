fc9a529733c36143e942707e75ee9949a046c8cd
elenaslavutina <elena.slavutina2013@gmail.com>
2020-06-14 16:28:37 +0300

    validation tests are added

--- src/test/java/ru/netology/web/ValidationTest.java
+++ src/test/java/ru/netology/web/ValidationTest.java
@@ -2,6 +2,7 @@ package ru.netology.web;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static com.codeborne.selenide.Selenide.*;
@@ -23,7 +24,6 @@ class ValidationTest {
     @Test
     void shouldNotPassedIfNameEmpty() {
         open("http://localhost:9999");
-        //     $(By.xpath("//*[@id=\"root\"]/div/form/div[1]/span/span/span[2]/input")).sendKeys("Василий___Пупкин");
         $("[data-test-id=phone] input").sendKeys("+71231234123");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
@@ -39,7 +39,6 @@ class ValidationTest {
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         String actual = $(By.xpath("//*[@id=\"root\"]/div/form/div[2]/span/span/span[3]")).getText();
-        //String actual = $("[data-test-id=phone]").$("[.input_invalid]").$(".input__sub").getText();
         assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", actual.trim());
     }
 
@@ -48,13 +47,20 @@ class ValidationTest {
     void shouldNotPassedIfPhoneEmpty() {
         open("http://localhost:9999");
         $(By.xpath("//*[@id=\"root\"]/div/form/div[1]/span/span/span[2]/input")).sendKeys("Василий Пупкин");
-        // $("[data-test-id=phone] input").sendKeys("71231234123");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         String actual = $(By.xpath("//*[@id=\"root\"]/div/form/div[2]/span/span/span[3]")).getText();
-        //String actual = $("[data-test-id=phone]").$("[.input_invalid]").$(".input__sub").getText();
         assertEquals("Поле обязательно для заполнения", actual.trim());
-    }
+    };
 
-    ;
+    @Test
+    void shouldNotPassedIfAgreementNotChecked() {
+        open("http://localhost:9999");
+        $(By.xpath("//*[@id=\"root\"]/div/form/div[1]/span/span/span[2]/input")).sendKeys("Василий Пупкин");
+        $("[data-test-id=phone] input").sendKeys("+71231234123");
+        $("button.button").click();
+        String actual = $("[data-test-id=agreement].input_invalid").getText();
+
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", actual.trim());
+    }
 }
