71fb9810bc2901b522fe498835b354268d8e0b7e
elenaslavutina <elena.slavutina2013@gmail.com>
2020-06-14 15:43:52 +0300

    appveyor.yml is updated to fix problem

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -2,6 +2,7 @@ package ru.netology.web;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,13 +19,17 @@ class CallbackTest {
     @Test
     void shouldTestV1() {
         open("http://localhost:9999");
-        $("form.form").$("[data-test-id=name]input").sendKeys("Василий Пупкин");
+        //$("form.form").$("[data-test-id=name]input").sendKeys("Василий Пупкин");
+        $(By.xpath("//*[@id=\"root\"]/div/form/div[1]/span/span/span[2]/input")).sendKeys("Василий Пупкин");
+
+
         $("[data-test-id=phone] input").sendKeys("+71231234123");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         String actual = $("[data-test-id=order-success]").getText();
 
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actual.trim());
+
     }
 
 
